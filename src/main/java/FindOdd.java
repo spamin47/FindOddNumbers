28588bb1ad865b2f1cf63edb185e220226ca2f01
Minh Nguyen <mnguyen4725@gmail.com>
2022-09-04 14:49:00 -0700

    added comments

--- src/main/java/FindOdd.java
+++ src/main/java/FindOdd.java
@@ -4,12 +4,14 @@ public class FindOdd {
     public static int[] SpOdd(int[] arr){
         int len = arr.length;
         int indx = 0;
+        //Save odd element into the original array
         for(int i = 0;i<len;i++){
             if((arr[i]%2)!=0){
                 arr[indx] = arr[i];
                 indx++;
             }
         }
+        //Transfer odd elements from original array to new array with fixed size (num of odds)
         int[] odd = new int[indx];
         for(int i = 0;i<indx;i++){
             odd[i] = arr[i];
@@ -18,6 +20,7 @@ public class FindOdd {
     }
 
     public static void main(String[] args){
+        //test case
         int[] nums = new int[]{3,8,5,7,1,9,2};
         int[] odd = SpOdd(nums);
         for(int num: odd){
